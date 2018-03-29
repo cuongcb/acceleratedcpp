d2b57ee93a0ad36bb5ab434a456b74bb067cee78
cuongcb <cuong300492@gmail.com>
2018-03-29 18:57:06 +0700

    Frame Creator

--- Chapter05/StringSplitter/src/Spliter.cpp
+++ Chapter05/StringSplitter/src/Spliter.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cctype> // isspace
 #include <iostream>
 #include <stdexcept>
@@ -8,6 +9,84 @@ using std::vector;		using std::string;
 using std::domain_error;	using std::cout;
 using std::endl;		using std::out_of_range;
 using std::getline;		using std::cin;
+using std::max;
+
+// return the max size in a collection of strings
+string::size_type width(const vector<string> &v)
+{
+	string::size_type maxlen = 0;
+	for (string it : v)
+		maxlen = max(maxlen, it.size());
+	return maxlen;
+}
+
+// create a frame of the current strings
+
+/*
+********
+* abcd *
+********
+*/
+vector<string> frame(const vector<string> &v)
+{
+	vector<string> ret;
+	string::size_type maxlen = width(v) + 4;
+
+	ret.push_back(string(maxlen, '*'));
+	for (vector<string>::const_iterator it = v.begin(); it != v.end(); it++)
+	{
+		ret.push_back("* " + *it + string(maxlen - (*it).size(), ' ') + " *");
+	}
+	ret.push_back(string(maxlen, '*'));
+
+	return ret;
+}
+
+vector<string> vcat(const vector<string> &top, const vector<string> &bot)
+{
+	vector<string> ret = top;
+	ret.insert(ret.end(), bot.begin(), bot.end());
+
+	return ret;
+}
+
+vector<string> hcat(const vector<string> &left, const vector<string> &right)
+{
+	vector<string> ret;
+	typedef vector<string>::size_type vec_sz;
+	string::size_type swidth = width(left) + 1;
+	vec_sz lsize = left.size();
+	vec_sz rsize = right.size();
+	vec_sz i = 0, j = 0;
+
+	while (i != lsize || j != rsize)
+	{
+		string s;
+#if 0
+		if (lsize != 0)
+		{
+			ret.push_back(left[i++]);
+		}
+		else
+		{
+			ret.push_back(string(left[0].size(), ' '));
+		}
+
+		ret.push_back(right[j++]);
+#endif
+		if (i != lsize)
+			s = left[i++];
+
+		s += string(swidth - s.size(), ' ');
+
+		if (j != rsize)
+			s += right[j++];
+
+		ret.push_back(s);
+	}
+
+	return ret;
+}
 
 vector<string> split(const string &s, char delimiter)
 {
